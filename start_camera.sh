04cea224cc4c3d185f3a0c3dbbbc156d5185d1d2
student2017 <student2017@localhost.localdomain>
2017-07-13 15:28:37 +0900

    changed to use mjpg-streamer

--- start_camera.sh
+++ start_camera.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 if [ -e /dev/video0 ]; then
-	/usr/bin/vlc -f v4l2:///dev/video0
+    /usr/local/bin/mjpg_streamer -i "/usr/local/lib/input_uvc.so -f 30 -r 1280x720 -d /dev/video0 -y -n" -o "/usr/local/lib/output_http.so -w /usr/local/www -p 8080" &
+    /usr/bin/vlc -f http://localhost:8080/?action=stream
+    #/usr/bin/vlc -f v4l2:///dev/video0
 fi
