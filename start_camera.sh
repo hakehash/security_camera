1bc685b4176b8e2a3bccbafd9957c6b176922b86
hakehash <b7dedc8797ec8ty@gmail.com>
2017-08-03 22:57:08 +0900

    minor change

--- start_camera.sh
+++ start_camera.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 if [ -e /dev/video0 ]; then
-    /usr/local/bin/mjpg_streamer -i "/usr/local/lib/input_uvc.so -f 30 -r 1280x720 -d /dev/video0 -y -n" -o "/usr/local/lib/output_http.so -w /usr/local/www -p 8080" &
-    /usr/bin/vlc -f http://localhost:8080/?action=stream
+    /usr/local/bin/mjpg_streamer \
+    -i "/usr/local/lib/input_uvc.so -f 30 -r 1280x720 -d /dev/video0 -y -n" \
+    -o "/usr/local/lib/output_http.so -w /usr/local/www -p 8080" &
+    #/usr/bin/vlc -f http://localhost:8080/?action=stream
     #/usr/bin/vlc -f v4l2:///dev/video0
 fi
